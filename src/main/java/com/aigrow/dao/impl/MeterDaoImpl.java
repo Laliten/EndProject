57affb2b99c6db901e2580f37c1f19dfdd8567b5
1176547971 <1176547971@qq.com>
2018-12-27 17:48:24 +0800

    修改了代码格式

--- src/main/java/com/aigrow/dao/impl/MeterDaoImpl.java
+++ src/main/java/com/aigrow/dao/impl/MeterDaoImpl.java
@@ -2,11 +2,14 @@ package com.aigrow.dao.impl;
 
 import com.aigrow.dao.MeterDao;
 import com.aigrow.model.entity.Meter;
+import org.hibernate.SQLQuery;
 import org.springframework.stereotype.Repository;
 
-/**
- * @author YangDeJian
- */
+import java.util.List;
+import java.util.Map;
+
+
 @Repository
-public class MeterDaoImpl extends BaseDaoImpl<Meter> implements MeterDao {
+public class MeterDaoImpl extends BaseDaoImpl<Meter> implements MeterDao{
+
 }
